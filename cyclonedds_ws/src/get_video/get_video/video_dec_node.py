27c33ede8e3f66d2ce1ced77d02859f6141993eb
Vadzim Selivestrau <selivestrau2003@gmail.com>
2026-09-15 11:48:13 +0300

    fix video dec node to jetson

--- cyclonedds_ws/src/get_video/get_video/video_dec_node.py
+++ cyclonedds_ws/src/get_video/get_video/video_dec_node.py
@@ -1,12 +1,23 @@
+import threading
+
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
+import numpy as np
+
 import gi
 gi.require_version('Gst', '1.0')
 gi.require_version('GstApp', '1.0')
-from gi.repository import Gst, GstApp, GLib
-import numpy as np
+gi.require_version('GstRtspServer', '1.0')
+from gi.repository import Gst, GstApp, GstRtspServer, GLib
+
+
+MULTICAST_ADDR = '230.1.1.1'
+MULTICAST_PORT = 1720
+IFACE = 'eno1'
+RTSP_PORT = '8554'
+RTSP_MOUNT = '/stream'
 
 
 class VideoStreamNode(Node):
@@ -18,18 +29,43 @@ class VideoStreamNode(Node):
         Gst.init(None)
 
         pipeline_str = (
-            'udpsrc address=230.1.1.1 port=1720 multicast-iface=enp4s0 ! '
+            f'udpsrc address={MULTICAST_ADDR} port={MULTICAST_PORT} multicast-iface={IFACE} ! '
             'queue ! application/x-rtp, media=video, encoding-name=H264 ! '
             'rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! '
             'video/x-raw, format=BGR ! appsink name=sink emit-signals=true'
         )
-
         self.pipeline = Gst.parse_launch(pipeline_str)
         self.appsink = self.pipeline.get_by_name('sink')
         self.appsink.connect('new-sample', self.on_new_sample)
-
         self.pipeline.set_state(Gst.State.PLAYING)
-        self.get_logger().info('Video stream started: udp://230.1.1.1:1720')
+        self.get_logger().info('ROS image publishing started: /sobaka_image')
+
+
+        self.rtsp_server = GstRtspServer.RTSPServer()
+        self.rtsp_server.set_service(RTSP_PORT)
+
+        factory = GstRtspServer.RTSPMediaFactory()
+        factory.set_launch(
+            f'( udpsrc address={MULTICAST_ADDR} port={MULTICAST_PORT} multicast-iface={IFACE} ! '
+            'application/x-rtp, media=video, encoding-name=H264 ! '
+            'rtph264depay ! h264parse config-interval=1 ! '
+            'queue max-size-buffers=1 leaky=downstream ! '
+            'rtph264pay name=pay0 pt=96 )'
+        )
+        factory.set_shared(True)  # один пайплайн на всех клиентов, не плодим сокеты
+        factory.set_latency(0)   # добавили эту строку
+        mounts = self.rtsp_server.get_mount_points()
+        mounts.add_factory(RTSP_MOUNT, factory)
+        self.rtsp_server.attach(None)
+
+        self.get_logger().info(
+            f'RTSP stream available at rtsp://<jetson_ip>:{RTSP_PORT}{RTSP_MOUNT}'
+        )
+
+
+        self.glib_loop = GLib.MainLoop()
+        self.glib_thread = threading.Thread(target=self.glib_loop.run, daemon=True)
+        self.glib_thread.start()
 
     def on_new_sample(self, sink):
         sample = sink.emit('pull-sample')
@@ -38,7 +74,6 @@ class VideoStreamNode(Node):
 
         buf = sample.get_buffer()
         caps = sample.get_caps()
-
         structure = caps.get_structure(0)
         width = structure.get_int('width')[1]
         height = structure.get_int('height')[1]
@@ -58,6 +93,8 @@ class VideoStreamNode(Node):
 
     def destroy_node(self):
         self.pipeline.set_state(Gst.State.NULL)
+        if self.glib_loop.is_running():
+            self.glib_loop.quit()
         super().destroy_node()
 
 
